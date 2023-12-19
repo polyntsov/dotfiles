a6bf9ccc384c9b8069f577344170cf6ada08b79b
Michael Polyntsov <Arno9148@gmail.com>
2023-12-19 03:32:15 +0300

    Make toggl polybar modules disappear when toggl returns an error

--- .config/polybar/toggl_proj.sh
+++ .config/polybar/toggl_proj.sh
@@ -1,11 +1,21 @@
 #!/bin/zsh
 
 function current_task () {
-    task=$(toggl ls --today -f 'project, description, stop' | \
-        grep running | \
+    task=$(toggl ls --today -f 'project, description, stop' 2>/dev/null)
+    ret_code=$?
+
+    if [ $ret_code -ne 0 ]; then
+        return
+    fi
+
+    task=$(echo "$task" | grep running | \
         awk -F' \\(#|\\)  |  ' '{project=$1; $1=""; $2=""; sub(/^[ \t]+/, "", $0); print project ": " substr($0, 0, length($0)-length($NF)-1)}' | \
         sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
-    echo "$task"
+    if [ -z "$task" ]; then
+        echo "Doing nothing; the ethical life is the productive life (c)"
+    else
+        echo "$task"
+    fi
 }
 
 current_task
--- .config/polybar/toggl_time.sh
+++ .config/polybar/toggl_time.sh
@@ -1,8 +1,18 @@
 #!/bin/zsh
 
 function work_time () {
-    time=$(toggl sum -t | grep today | awk -F' ' '{print $2}' | awk -F':' '{print $1":"$2}')
-    echo "$time"
+    time=$(toggl sum -t 2>/dev/null)
+    ret_code=$?
+
+    if [ $ret_code -ne 0 ]; then
+        return
+    fi
+
+    time=$(echo "$time" | grep today | awk -F' ' '{print $2}' | awk -F':' '{print $1":"$2}')
+    if [ -z "$time" ]; then
+        time="0:00"
+    fi
+    echo "$time hrs"
 }
 
 work_time
