9f5b0abfcffd430e83f278b2da6a6a6a50b16aed
Michael Polyntsov <arno9148@gmail.com>
2026-02-22 16:20:37 +0000

    Configure tmux

--- hosts/elise/home.nix
+++ hosts/elise/home.nix
@@ -6,6 +6,7 @@
       ../../modules/home-manager/alacritty.nix
       ../../modules/home-manager/starship.nix
       ../../modules/home-manager/zsh.nix
+      ../../modules/home-manager/tmux.nix
     ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
