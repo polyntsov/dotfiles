bab1b5b8f319ef129b8d922a9f44654a257f3edf
Michael Polyntsov <Arno9148@gmail.com>
2026-03-01 12:29:18 +0000

    Change colours, install zoom

--- hosts/elise/home.nix
+++ hosts/elise/home.nix
@@ -31,6 +31,7 @@
     gemini-cli
     pixel-code
     discord
+    zoom-us
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
