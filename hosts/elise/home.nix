3c80777f4583ba9736ad5b1741b5fdbc300d663a
Michael Polyntsov <arno9148@gmail.com>
2026-03-14 19:11:50 +0000

    Add opencode package

--- hosts/elise/home.nix
+++ hosts/elise/home.nix
@@ -42,6 +42,7 @@
     gemini-cli
     discord
     zoom-us
+    opencode
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
