c9a0bbb88b8849c590bdeee28cc4552fd34096c5
Michael Polyntsov <arno9148@gmail.com>
2026-03-20 13:26:22 +0000

    Add ascii-draw package

--- hosts/elise/home.nix
+++ hosts/elise/home.nix
@@ -44,6 +44,7 @@
     zoom-us
     opencode
     element-desktop
+    ascii-draw
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
