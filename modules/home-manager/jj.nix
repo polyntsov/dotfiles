aa5a39bb74067562e356ebedcec820e70455b2d6
Michael Polyntsov <arno9148@gmail.com>
2026-03-22 18:48:06 +0000

    Update jj config

--- modules/home-manager/jj.nix
+++ modules/home-manager/jj.nix
@@ -8,11 +8,63 @@
         name = userSettings.name;
         email = userSettings.email;
       };
+
       ui = {
         editor = "nvim";
         default-command = "status";
         paginate = "auto";
         pager = "less -FRX";
+      };
+
+      revset-aliases = {
+        "closest_bookmark(to)" = "heads(::to & bookmarks())";
+        "immutable_heads()" = "none()";
+      };
+
+      aliases = {
+        push = [
+          "git"
+          "push"
+        ];
+        tug = [
+          "bookmark"
+          "move"
+          "--from"
+          "closest_bookmark(@-)"
+          "--to"
+          "@-"
+        ];
+        my = [
+          "log"
+          "-r"
+          "mine()"
+        ];
+        ml = [
+          "log"
+          "-r"
+          "visible_heads() & mine()"
+        ];
+        lf = [
+          "log"
+          "-r"
+          "visible_heads()"
+        ];
+        blame = [
+          "file"
+          "annotate"
+        ];
+        sweep = [
+          "abandon"
+          "divergent() ~ ancestors(@)"
+        ];
+      };
+
+      templates = {
+        git_push_bookmark = "\"misha/push\" ++ change_id.short()";
+      };
+
+      remotes.origin = {
+        auto-track-bookmarks = "glob:*";
       };
     };
   };
