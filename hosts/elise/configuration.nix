f0e89e0ea83bd09705589ca31ca97072bb5c68c9
Michael Polyntsov <arno9148@gmail.com>
2026-04-05 14:33:00 +0100

    Revert "Update kernel, nvidia"
    This reverts commit 30fefd55870ea65e661fd05fc3f4d0e23f3a49d2.

--- hosts/elise/configuration.nix
+++ hosts/elise/configuration.nix
@@ -123,7 +123,7 @@ in
   system.stateVersion = "25.11"; # Did you read the comment?
 
   # Kernel
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_6_18;
 
   boot.loader.systemd-boot.configurationLimit = 3;
 
@@ -133,6 +133,15 @@ in
     "flakes"
   ];
   nixpkgs.config.allowUnfree = true;
+
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    version = "580.82.09";
+    sha256_64bit = "sha256-Puz4MtouFeDgmsNMKdLHoDgDGC+QRXh6NVysvltWlbc=";
+    sha256_aarch64 = "sha256-6tHiAci9iDTKqKrDIjObeFdtrlEwjxOHJpHfX4GMEGQ=";
+    openSha256 = "sha256-YB+mQD+oEDIIDa+e8KX1/qOlQvZMNKFrI5z3CoVKUjs=";
+    settingsSha256 = "sha256-um53cr2Xo90VhZM1bM2CH4q9b/1W2YOqUcvXPV6uw2s=";
+    persistencedSha256 = "sha256-lbYSa97aZ+k0CISoSxOMLyyMX//Zg2Raym6BC4COipU=";
+  };
 
   services.envfs.enable = true;
   virtualisation.docker = {
