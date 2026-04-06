a6cff94f8c9556e5b2e58ecd52164bd54b765118
Michael Polyntsov <arno9148@gmail.com>
2026-04-06 17:52:40 +0100

    Add stars theme

---
+++ themes/stars.nix
@@ -0,0 +1,39 @@
+# Stars -- night mountain scene, dark blue accent
+{
+  name = "stars";
+
+  bg = "#0d1117";
+
+  wallpaper = ../media/pic/wall/stars.jpg;
+
+  animation = ../media/animations;
+
+  font = {
+    family = "Pixel Code";
+    package = "pixel-code";
+    size = 10;
+  };
+
+  neovim = {
+    colorscheme = "carbonfox";
+    background = "dark";
+  };
+
+  alacritty = {
+    theme = "carbonfox";
+  };
+
+  gnome = {
+    colorScheme = "prefer-dark";
+    accentColor = "blue";
+    borderColor = "rgba(26, 95, 160, 0.85)";
+  };
+
+  tmux = {
+    bg = "#0d1117";
+    fg = "#c0cfe0";
+    accent = "#1a5fa0";
+    accentAlt = "#2471a3";
+    inactive = "#3d4f5e";
+  };
+}
