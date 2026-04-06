3b6b0194cbdff7cf025b0ffb7bf807ed741d236c
Michael Polyntsov <misha@symbolica.ai>
2026-04-06 18:25:07 +0100

    Ignore only specific dot dirs in telescope

--- modules/home-manager/neovim/lua/plugins/telescope.lua
+++ modules/home-manager/neovim/lua/plugins/telescope.lua
@@ -1,5 +1,19 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ff', function()
+  builtin.find_files({
+    find_command = {
+      'ag', '--hidden',
+      '--ignore-dir', '.local',
+      '--ignore-dir', '.git',
+      '--ignore-dir', '.mozilla',
+      '--ignore-dir', '.thunderbird',
+      '--ignore-dir', '.dotfiles',
+      '--ignore-dir', '.cache',
+      '--ignore-dir', '.jj',
+      '-g', '',
+    },
+  })
+end, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
