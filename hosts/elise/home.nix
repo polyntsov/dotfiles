c0580b7a76b30e5a2a31beadd4dd3321a7c6efa6
Michael Polyntsov <arno9148@gmail.com>
2026-04-06 12:29:52 +0100

    Add claude-code

--- hosts/elise/home.nix
+++ hosts/elise/home.nix
@@ -45,6 +45,7 @@
     cursor-cli
     file
     signal-desktop
+    claude-code
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
