9b908f6673d2d1b058fda5ad760d5465356c1e33
Michael Polyntsov <arno9148@gmail.com>
2026-07-09 19:59:10 +0100

    Set pipewire to high-res

--- modules/common/pipewire.nix
+++ modules/common/pipewire.nix
@@ -20,5 +20,17 @@
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
+
+    extraConfig.pipewire."10-clock-rate" = {
+      "context.properties" = {
+        "default.clock.rate" = 96000;
+        "default.clock.allowed-rates" = [
+          44100
+          48000
+          88200
+          96000
+        ];
+      };
+    };
   };
 }
