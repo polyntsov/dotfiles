625d1e216bf047d8fb1a63ddd8b95466388482e0
Michael Polyntsov <arno9148@gmail.com>
2026-07-18 17:34:38 +0100

    Add feh

--- hosts/elise/home.nix
+++ hosts/elise/home.nix
@@ -53,6 +53,7 @@
     signal-desktop
     claude-code
     google-chrome
+    feh
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
